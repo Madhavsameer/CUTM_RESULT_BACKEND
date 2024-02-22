cbddf1887d0162f275a3c3e22728ef5eafb539d1
Madhav sameer <111692125+Madhavsameer@users.noreply.github.com>
2024-02-23 02:30:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 // Create Express app
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://cutm-result-frontend.onrender.com/' }));
 
 // Connect to MongoDB
 connectDB();
